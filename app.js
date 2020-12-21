8a8d5057eac9c24638e55799bbbb2d954ad2561d
Carlstong <carlstongrefalde@gmail.com>
2020-12-21 21:13:58 +0800

    patch v2 added

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+
+var toggle  = document.getElementById("toggle");
+var content = document.getElementById("content");
+
+toggle.addEventListener("click", function() {
+  content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
+});
+
+var toggle = document.getElementById("toggle2")
+var content2 = document.getElementById("content2")
+
+toggle.addEventListener("click", function() {
+    content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
+  });
